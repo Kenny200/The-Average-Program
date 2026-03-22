3084abec3f2fb732b7aa5db8e37e609111a70b82
Kenny200 <58740689+Kenny200@users.noreply.github.com>
2026-03-22 12:28:58 -0400

    Create the_app.py for average calculation
    Add initial implementation with average calculation.

---
+++ the_app.py
@@ -0,0 +1,9 @@
+# 5/20/2025
+
+from average import calculateAverage
+
+myDict = {"B" : 19.4, "W" : 19.8, "H" : 18.4, "AS" : 9.8}
+
+#user = input()
+for value in myDict.values():
+        print(calculateAverage(19.4))
